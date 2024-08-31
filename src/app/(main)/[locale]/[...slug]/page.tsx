89a873165c6e665d52f8d0b7ff5ed7ac14c9fb3a
adolfokrah <adolfokrah@gmail.com>
2024-08-31 21:07:15 +0200

    fix: updated visio-cms

--- src/app/(main)/[locale]/[...slug]/page.tsx
+++ src/app/(main)/[locale]/[...slug]/page.tsx
@@ -1,14 +1,39 @@
 export const dynamic = 'force-dynamic'
 export const fetchCache = 'force-no-store'
-import { getPageBlocks } from 'visio-cms-lib/utils'
+import { getPageBlocks, getPageMetaData } from 'visio-cms-lib/utils'
 import PageContent from '../../page-content'
 import NotFound from '@/src/app/not-found'
+import type { Metadata } from 'next'
 
-export default async function Page({
-  params,
-}: {
+type PageProps = {
   params: { slug: string[]; locale: string }
-}) {
+}
+
+export async function generateMetadata({
+  params,
+}: PageProps): Promise<Metadata> {
+  const { slug, locale } = params
+
+  const pageMetaData = await getPageMetaData(
+    `/${slug.join('/')}`,
+    process.env.NEXT_PUBLIC_SUPABASE_ANONKEY || '',
+    process.env.NEXT_PUBLIC_SUPABASE_URL || '',
+    locale,
+  )
+
+  return {
+    title: pageMetaData.title,
+    description: pageMetaData.description,
+    keywords: pageMetaData.keywords,
+    openGraph: {
+      title: pageMetaData.title,
+      description: pageMetaData.description,
+      images: [pageMetaData?.featuredImage || ''],
+    },
+  }
+}
+
+export default async function Page({ params }: PageProps) {
   const { slug, locale } = params
   const data = await getPageBlocks(
     `/${slug.join('/')}`,
